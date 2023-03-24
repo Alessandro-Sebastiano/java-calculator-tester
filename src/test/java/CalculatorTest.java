5873d37012340c0411720fda5a154ada5ed9d249
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-24 15:24:17 +0100

    class-calculator-test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    void add() {
+        assertEquals(0, 5 + -5);
+    }
+
+    @Test
+    void subtract() {
+        assertEquals(12, 32 - 20);
+    }
+
+    @Test
+    void divide() {
+        try {
+            assertEquals(2, 12 / 0);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    void multiply() {
+        assertEquals(12, 6 * 2);
+    }
+
+}
